6769b18255392c87d7fff897d3cc65f4943531bd
Gianne Ola <gianneola@gmail.com>
2026-05-06 02:36:59 +0800

    readjusted layout sizes, 1920x1080p main basis for 2xl breakpoint as it is one of the most common monitor resolution. Contact section has also been completed along with its responsiveness. Next step is to work on sidebar for tablet-mobile view as well as small display laptops.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,19 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    borderRadius: {
-      'none' : '0px',
-      'sm' : '0.125rem',
-      'md' : '0.375rem',
-      'lg' : '0.5rem',
-      'xl' : '0.75rem',
-      '2xl' : '1rem',
-      '3xl' : '1.5rem',
-      'full' : '10rem'
+    extend: {
+      fontSize: {
+        'fluid-xs':   ['clamp(0.75rem, 1vw, 0.875rem)'],
+        'fluid-sm':   ['clamp(0.875rem, 1.5vw, 1rem)'],
+        'fluid-base': ['clamp(1rem, 2vw, 1.25rem)'],
+        'fluid-lg':   ['clamp(1.125rem, 2.5vw, 1.5rem)'],
+        'fluid-xl':   ['clamp(1.25rem, 3vw, 2rem)'],
+        'fluid-2xl':  ['clamp(1.5rem, 3.5vw, 2.5rem)'],
+        'fluid-3xl':  ['clamp(2rem, 4vw, 3rem)'],
+        'fluid-4xl':  ['clamp(2.5rem, 5vw, 4rem)'],
+        'fluid-5xl':  ['clamp(3rem, 6vw, 5rem)'],
+      }
     },
-    
-    extend: {},
   },
   plugins: [],
 }
